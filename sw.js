edd157740012c988f705d893c5dd0f1f4bb956ad
abigailhale-cmd <abigailhale@hopeacademyok.org>
2026-02-10 16:25:38 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,8 @@
-self.addEventListener("fetch", () => {});
+// Minimal service worker to make PWA installable
+self.addEventListener('install', event => {
+  console.log('Service Worker installed');
+});
+
+self.addEventListener('fetch', event => {
+  // Can be expanded for offline caching later
+});
