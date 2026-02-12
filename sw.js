d56436923bb69ca87773463c030dbcecaf084188
abigailhale-cmd <abigailhale@hopeacademyok.org>
2026-02-11 21:12:50 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,22 @@
 // Minimal service worker to make PWA installable
+
+// Install event
 self.addEventListener('install', event => {
   console.log('Service Worker installed');
+  // Activate immediately without waiting
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
-  // Can be expanded for offline caching later
+// Activate event
+self.addEventListener('activate', event => {
+  console.log('Service Worker activated');
+  // Take control of all pages immediately
+  event.waitUntil(self.clients.claim());
 });
+
+// Fetch event (optional, needed for install prompt)
+self.addEventListener('fetch', event => {
+  // You can add offline caching later
+  event.respondWith(fetch(event.request));
+});
+
